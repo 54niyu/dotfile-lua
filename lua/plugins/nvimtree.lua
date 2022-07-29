57852478387544b531d7f7dd5aab3b3ff5fa4866
bing.wang <bing.wang@mihoyo.com>
2022-07-29 23:52:32 +0800

    update nvimtree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -37,192 +37,112 @@ return function()
 
     -- following options are the default
     require("nvim-tree").setup({
-
-        filters = {
-            dotfiles = false,
-            -- exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
-        },
-        disable_netrw = true,
+        auto_reload_on_write = true,
+        disable_netrw = false,
+        -- hide_root_folder = false,
+        hijack_cursor = false,
         hijack_netrw = true,
-        open_on_setup = false,
-        ignore_ft_on_setup = { "alpha" },
-        hijack_cursor = true,
         hijack_unnamed_buffer_when_opening = false,
-        update_cwd = true,
-        update_focused_file = {
-            enable = true,
-            update_cwd = false,
-        },
+        ignore_buffer_on_setup = false,
+        open_on_setup = false,
+        open_on_setup_file = false,
+        open_on_tab = false,
+        sort_by = "name",
+        update_cwd = false,
         view = {
-            adaptive_size = true,
+            width = 30,
+            height = 30,
             side = "left",
-            width = 25,
-            hide_root_folder = true,
+            preserve_window_proportions = false,
+            number = false,
+            relativenumber = false,
+            signcolumn = "yes",
             mappings = {
                 custom_only = false,
                 list = list,
             },
         },
-        git = {
-            enable = false,
-            ignore = true,
-        },
-        filesystem_watchers = {
-            enable = true,
-        },
-        actions = {
-            open_file = {
-                resize_window = true,
-            },
-        },
         renderer = {
-            highlight_git = false,
-            highlight_opened_files = "none",
-
             indent_markers = {
                 enable = false,
-            },
-
-            icons = {
-                show = {
-                    file = true,
-                    folder = true,
-                    folder_arrow = true,
-                    git = false,
+                icons = {
+                    corner = "└ ",
+                    edge = "│ ",
+                    none = "  ",
                 },
-
-                glyphs = {
-                    default = "",
-                    symlink = "",
-                    folder = {
-                        default = "",
-                        empty = "",
-                        empty_open = "",
-                        open = "",
-                        symlink = "",
-                        symlink_open = "",
-                        arrow_open = "",
-                        arrow_closed = "",
-                    },
-                    git = {
-                        unstaged = "✗",
-                        staged = "✓",
-                        unmerged = "",
-                        renamed = "➜",
-                        untracked = "★",
-                        deleted = "",
-                        ignored = "◌",
+            },
+            icons = {
+                webdev_colors = true,
+            },
+        },
+        hijack_directories = {
+            enable = true,
+            auto_open = true,
+        },
+        update_focused_file = {
+            enable = false,
+            update_cwd = false,
+            ignore_list = {},
+        },
+        ignore_ft_on_setup = {},
+        system_open = {
+            cmd = nil,
+            args = {},
+        },
+        diagnostics = {
+            enable = false,
+            show_on_dirs = false,
+            icons = {
+                hint = "",
+                info = "",
+                warning = "",
+                error = "",
+            },
+        },
+        filters = {
+            dotfiles = false,
+            custom = {},
+            exclude = {},
+        },
+        git = {
+            enable = true,
+            ignore = true,
+            timeout = 400,
+        },
+        actions = {
+            use_system_clipboard = true,
+            change_dir = {
+                enable = true,
+                global = false,
+            },
+            open_file = {
+                quit_on_open = false,
+                resize_window = false,
+                window_picker = {
+                    enable = true,
+                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                    exclude = {
+                        filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                        buftype = { "nofile", "terminal", "help" },
                     },
                 },
             },
         },
-
-        -- auto_reload_on_write = true,
-        -- disable_netrw = false,
-        -- -- hide_root_folder = false,
-        -- hijack_cursor = false,
-        -- hijack_netrw = true,
-        -- hijack_unnamed_buffer_when_opening = false,
-        -- ignore_buffer_on_setup = false,
-        -- open_on_setup = false,
-        -- open_on_setup_file = false,
-        -- open_on_tab = false,
-        -- sort_by = "name",
-        -- update_cwd = false,
-        -- view = {
-        -- 	width = 30,
-        -- 	height = 30,
-        -- 	side = "left",
-        -- 	preserve_window_proportions = false,
-        -- 	number = false,
-        -- 	relativenumber = false,
-        -- 	signcolumn = "yes",
-        -- 	mappings = {
-        -- 		custom_only = false,
-        -- 		list = list,
-        -- 	},
-        -- },
-        -- renderer = {
-        -- 	indent_markers = {
-        -- 		enable = false,
-        -- 		icons = {
-        -- 			corner = "└ ",
-        -- 			edge = "│ ",
-        -- 			none = "  ",
-        -- 		},
-        -- 	},
-        -- 	icons = {
-        -- 		webdev_colors = true,
-        -- 	},
-        -- },
-        -- hijack_directories = {
-        -- 	enable = true,
-        -- 	auto_open = true,
-        -- },
-        -- update_focused_file = {
-        -- 	enable = false,
-        -- 	update_cwd = false,
-        -- 	ignore_list = {},
-        -- },
-        -- ignore_ft_on_setup = {},
-        -- system_open = {
-        -- 	cmd = nil,
-        -- 	args = {},
-        -- },
-        -- diagnostics = {
-        -- 	enable = false,
-        -- 	show_on_dirs = false,
-        -- 	icons = {
-        -- 		hint = "",
-        -- 		info = "",
-        -- 		warning = "",
-        -- 		error = "",
-        -- 	},
-        -- },
-        -- filters = {
-        -- 	dotfiles = false,
-        -- 	custom = {},
-        -- 	exclude = {},
-        -- },
-        -- git = {
-        -- 	enable = true,
-        -- 	ignore = true,
-        -- 	timeout = 400,
-        -- },
-        -- actions = {
-        -- 	use_system_clipboard = true,
-        -- 	change_dir = {
-        -- 		enable = true,
-        -- 		global = false,
-        -- 	},
-        -- 	open_file = {
-        -- 		quit_on_open = false,
-        -- 		resize_window = false,
-        -- 		window_picker = {
-        -- 			enable = true,
-        -- 			chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        -- 			exclude = {
-        -- 				filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-        -- 				buftype = { "nofile", "terminal", "help" },
-        -- 			},
-        -- 		},
-        -- 	},
-        -- },
-        -- trash = {
-        -- 	cmd = "trash",
-        -- 	require_confirm = true,
-        -- },
-        -- log = {
-        -- 	enable = false,
-        -- 	truncate = false,
-        -- 	types = {
-        -- 		all = false,
-        -- 		config = false,
-        -- 		copy_paste = false,
-        -- 		diagnostics = false,
-        -- 		git = false,
-        -- 		profile = false,
-        -- 	},
-        -- },
+        trash = {
+            cmd = "trash",
+            require_confirm = true,
+        },
+        log = {
+            enable = false,
+            truncate = false,
+            types = {
+                all = false,
+                config = false,
+                copy_paste = false,
+                diagnostics = false,
+                git = false,
+                profile = false,
+            },
+        },
     })
 end
